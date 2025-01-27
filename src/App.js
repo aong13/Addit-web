a81b05750e596e90995b6f807de7b6e7cd7bcf04
aong13 <oaoa13@pukyong.ac.kr>
2025-01-28 03:58:03 +0900

    🏗️ build: 기본 라우팅 및 css 설정
    - 모바일 뷰 기준으로 개발할 수 있도록 함

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
+import AppRouter from "./router";
+import styled from "styled-components";
 import "./reset.css";
 
 function App() {
-  return <div className="App">addit web</div>;
+  return (
+    <AppContainer>
+      <AppRouter />
+    </AppContainer>
+  );
 }
+
+const AppContainer = styled.div`
+  width: 100%;
+  max-width: 480px; /* 최대 화면 크기 */
+  margin: 0 auto;
+  background-color: #f9f9f9;
+  min-height: 100vh;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
 
 export default App;
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Upload from "./pages/Upload";
+import Alarm from "./pages/Alarm";
+import MyPage from "./pages/MyPage";
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/alarm" element={<Alarm />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
