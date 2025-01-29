222aba88f81351b968c9a67bb4ebb26b8a9d0c8e
aong13 <oaoa13@pukyong.ac.kr>
2025-01-29 17:36:42 +0900

    :sparkles: feat: 프로필과 닉네임 추가

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -21,6 +21,13 @@ const Carousel = ({ data }) => {
         {data.flatMap((relay) =>
           relay.tickles.map((tickle, index) => (
             <ImageWrapper key={tickle.tickleId || `tickle-${index}`}>
+              <ProfileWrapper>
+                <ProfileImage
+                  src={tickle.profileImage || "/default-profile.jpg"}
+                  alt={`Profile ${tickle.tickleId || index}`}
+                />
+                <Nickname>{tickle.nickname || "No Name"}</Nickname>
+              </ProfileWrapper>
               <Image
                 src={tickle.thumbnail}
                 alt={`Slide ${tickle.tickleId || index}`}
@@ -40,7 +47,7 @@ const CarouselContainer = styled.div`
 `;
 
 const ImageWrapper = styled.div`
-  display: flex;
+  position: relative;
   justify-content: center;
   align-items: center;
   transition: transform 0.3s ease, opacity 0.3s ease;
@@ -56,9 +63,38 @@ const ImageWrapper = styled.div`
   }
 `;
 
+const ProfileWrapper = styled.div`
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 10px;
+  z-index: 1;
+`;
+
 const Image = styled.img`
   max-height: 400px;
   object-fit: contain;
   border-radius: 15px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.3);
+  width: 100%;
+`;
+
+const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  object-fit: cover;
+  border: 2px solid #ffffff;
+`;
+
+const Nickname = styled.span`
+  margin-top: 8px;
+  font-size: 0.625rem;
+  font-weight: 600;
+  color: #eeeeee;
+  text-shadow: 0px 0px 3px rgba(0, 0, 0, 0.5); /* 그림자 추가 */
 `;
