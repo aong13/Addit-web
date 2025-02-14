9192773da5d137d883e682380cb9961b1a29a617
aong13 <oaoa13@pukyong.ac.kr>
2025-02-14 15:55:28 +0900

    :bug: fix: 길이 계산 시 로딩

--- src/components/common/ImgWithBlur.jsx
+++ src/components/common/ImgWithBlur.jsx
@@ -1,30 +1,34 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import defaultThumbnail from "../../assets/icons/error_img.png";
 import { calculateImgRatio } from "../../utils/relayUtils";
 
 const ImgWithBlur = ({ imageSrc }) => {
-  const [fillBlur, setFillBlur] = useState(false);
+  const [fillBlur, setFillBlur] = useState(null);
 
   useEffect(() => {
     if (imageSrc) {
       calculateImgRatio(imageSrc, (ratio) => {
-        setFillBlur(ratio <= 3 / 4); // 3:4보다 크면 blur 적용
+        setFillBlur(ratio <= 3 / 4); // 3:4보다 길면 blur
       });
     }
   }, [imageSrc]);
 
+  //로딩상태
+  if (fillBlur === null) {
+    return <Thumbnail className="loading" />;
+  }
+
   return (
     <>
       <Thumbnail
-        src={imageSrc || defaultThumbnail}
+        src={imageSrc}
         alt="thumbnail"
         $fill={fillBlur}
         loading="lazy"
       />
       {!fillBlur && (
         <BlurBackground
-          src={imageSrc || defaultThumbnail}
+          src={imageSrc}
           alt="blurred background"
           loading="lazy"
         />
@@ -46,8 +50,12 @@ const BlurBackground = styled.img`
 
 const Thumbnail = styled.img`
   position: relative;
-  /* width: 100%; */
+  width: 100%;
   object-fit: ${({ $fill }) => ($fill ? "cover" : "contain")};
   aspect-ratio: 9 / 16;
   z-index: 1;
+
+  &.loading {
+    background-color: #00000020;
+  }
 `;
